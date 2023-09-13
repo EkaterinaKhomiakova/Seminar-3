f830ecdace53f9799a2aa6ecc92a3dcd40f7512c
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-13 16:37:45 +0300

    Task2

---
+++ Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая по заданному номеру
+// четверти показывавает диапозон возможных координат
+// точек в этой четверти(x и y)
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+Console.Write(" № = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N == 1)
+  Console.WriteLine("x > 0 , y > 0");
+else if (N == 2)
+  Console.WriteLine("x < 0 , y > 0");
+else if (N == 3)
+  Console.WriteLine("x < 0 , y < 0");
+else
+  Console.WriteLine("x > 0 , y < 0");
